4f0751de2500eb06159caf7e95a2f75c6d235f21
Dmitry Shatov <d.shatty@gmail.com>
2019-06-23 01:27:00 +0600

    Рефакторинг, обработка ошибок, освобождение ресурсов.

--- src/main.c
+++ src/main.c
@@ -30,47 +30,47 @@ static const int WINDOW_HEIGHT = 600;
 
 //----------------------------------------------------------------------------------------------------------------------
 
+typedef struct {
+    int c1;
+    int c2;
+    int bx;
+    int by;
+    bool l;
+    SDL_Rect b;
+    SDL_Rect p1;
+    SDL_Rect p2;
+} GameState;
 
-// TODO: избавиться от глобальных переменных.
-
-static int c1 = 0;
-static int c2 = 0;
-static SDL_Window *window;
-static SDL_Rect t;
-static SDL_Rect b;
-static SDL_Rect p1;
-static SDL_Rect p2;
-static TTF_Font *font;
-static SDL_Color color;
-static SDL_Surface *score;
-static SDL_Renderer *render;
-static SDL_Texture *background;
-static SDL_Texture *player_1;
-static SDL_Texture *player_2;
-static SDL_Texture *ball;
-static SDL_Texture *score2;
-static int bx = 4;
-static int by = 4;
-static bool l = true;
+typedef struct {
+    SDL_Window *window;
+    SDL_Rect t;
+    TTF_Font *font;
+    SDL_Color color;
+    SDL_Renderer *renderer;
+    SDL_Texture *backgroundTexture;
+    SDL_Texture *whiteTexture;
+} RenderObjects;
 
 //----------------------------------------------------------------------------------------------------------------------
 
 // TODO: Документация к методу.
 
-static void redraw() {
-    SDL_RenderClear(render);
-    SDL_RenderCopy(render, background, NULL, NULL);
-    SDL_RenderCopy(render, player_1, NULL, &p1);
-    SDL_RenderCopy(render, player_2, NULL, &p2);
-    SDL_RenderCopy(render, ball, NULL, &b);
+static void redraw(const RenderObjects *const ro, const GameState *const gs) {
+    SDL_RenderClear(ro->renderer);
+    SDL_RenderCopy(ro->renderer, ro->backgroundTexture, NULL, NULL);
+    SDL_RenderCopy(ro->renderer, ro->whiteTexture, NULL, &gs->p1);
+    SDL_RenderCopy(ro->renderer, ro->whiteTexture, NULL, &gs->p2);
+    SDL_RenderCopy(ro->renderer, ro->whiteTexture, NULL, &gs->b);
 
     char s[32];
-    snprintf(s, 32, "%d   %d", c1, c2);
-    score = TTF_RenderText_Solid(font, s, color);
-    score2 = SDL_CreateTextureFromSurface(render, score);
-    SDL_RenderCopy(render, score2, NULL, &t);
+    snprintf(s, 32, "%d   %d", gs->c1, gs->c2);
+    SDL_Surface *score = TTF_RenderText_Solid(ro->font, s, ro->color);
+    SDL_Texture *score2 = SDL_CreateTextureFromSurface(ro->renderer, score);
+    SDL_RenderCopy(ro->renderer, score2, NULL, &ro->t);
 
-    SDL_RenderPresent(render);
+    SDL_RenderPresent(ro->renderer);
+
+    SDL_DestroyTexture(score2);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -80,34 +80,100 @@ static void redraw() {
  * 
  * @return ноль в случае успеха; не ноль в случае ошибки.
  */
-static int initGraphic() {
+static int initRenderObjects(RenderObjects *const ro) {
+    memset(ro, 0, sizeof(*ro));
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    window = SDL_CreateWindow(
+    if (TTF_Init() < 0) {
+        printf("SDL_ttf could not initialize! TTF_Error: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    ro->window = SDL_CreateWindow(
         "PING PONG",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         WINDOW_WIDTH, WINDOW_HEIGHT,
         SDL_WINDOW_SHOWN
     );
-    if (NULL == window) {
+    if (NULL == ro->window) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // TODO: обрабатывать другие возможные ошибки инициализации.
+    ro->renderer = SDL_CreateRenderer(ro->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (NULL == ro->renderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
-    render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    background = SDL_CreateTextureFromSurface(render, SDL_LoadBMP("./background.bmp"));
-    player_1 = SDL_CreateTextureFromSurface(render, SDL_LoadBMP("./it.bmp"));
-    player_2 = SDL_CreateTextureFromSurface(render, SDL_LoadBMP("./it.bmp"));
-    ball = SDL_CreateTextureFromSurface(render, SDL_LoadBMP("./it.bmp"));
-    TTF_Init();
-    font = TTF_OpenFont("./16984.ttf", 12);
+    SDL_Surface *surface;
+
+    surface = SDL_LoadBMP("./background.bmp");
+    if (NULL == surface) {
+        printf("Background surface could not be loaded! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    ro->backgroundTexture = SDL_CreateTextureFromSurface(ro->renderer, surface);
+    SDL_FreeSurface(surface);
+    if (NULL == ro->backgroundTexture) {
+        printf("Background texture could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    surface = SDL_LoadBMP("./it.bmp");
+    if (NULL == surface) {
+        printf("White surface could not be loaded! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    ro->whiteTexture = SDL_CreateTextureFromSurface(ro->renderer, surface);
+    SDL_FreeSurface(surface);
+    if (NULL == ro->whiteTexture) {
+        printf("White texture could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    ro->font = TTF_OpenFont("./16984.ttf", 12);
+    if (NULL == ro->font) {
+        printf("Font could not be opened! TTF_Error: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    ro->color = (SDL_Color) {.r=225, .g=225, .b=225};
+    ro->t = (SDL_Rect) {.x=475, .y=40, .w=50, .h=40};
 
     return 0;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Освободить ресурсы, связанные с графикой.
+ */
+static void freeRenderObjects(RenderObjects *const ro) {
+    if (ro->font) {
+        TTF_CloseFont(ro->font);
+    }
+    if (ro->backgroundTexture) {
+        SDL_DestroyTexture(ro->backgroundTexture);
+    }
+    if (ro->whiteTexture) {
+        SDL_DestroyTexture(ro->whiteTexture);
+    }
+    if (ro->renderer) {
+        SDL_DestroyRenderer(ro->renderer);
+    }
+    if (ro->window) {
+        SDL_DestroyWindow(ro->window);
+    }
+
+    TTF_Quit();
+    SDL_Quit();
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -117,7 +183,7 @@ static int initGraphic() {
  * 
  * @param shouldExit true, если следует завершить работу программы.
  */
-static void pollSdlEvents(bool *shouldExit) {
+static void pollSdlEvents(bool *const shouldExit) {
     SDL_Event ev;
 
     *shouldExit = false;
@@ -144,12 +210,15 @@ static void pollSdlEvents(bool *shouldExit) {
 
 // TODO: Документация к методу.
 
-static void initGame() {
-    p1 = (SDL_Rect) {.x=0, .y=10, .w=20, .h=200};
-    p2 = (SDL_Rect) {.x=980, .y=10, .w=20, .h=200};
-    b = (SDL_Rect) {.x=490, .y=290, .w=20, .h=20};
-    t = (SDL_Rect) {.x=475, .y=40, .w=50, .h=40};
-    color = (SDL_Color) {.r=225, .g=225, .b=225};
+static void initGameState(GameState *const gs) {
+    *gs = (GameState) {
+        .p1 = (SDL_Rect) {.x=0, .y=10, .w=20, .h=200},
+        .p2 = (SDL_Rect) {.x=980, .y=10, .w=20, .h=200},
+        .b = (SDL_Rect) {.x=490, .y=290, .w=20, .h=20},
+        .c1 = 0, .c2 = 0,
+        .bx = 4, .by = 4,
+        .l = true
+    };
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -159,40 +228,40 @@ static void initGame() {
  *
  * @param dt время с момента последнего обновления (ms).
  */
-static void moveBall(Uint32 dt) {
+static void moveBall(const Uint32 dt, GameState *const gs) {
     // TODO: выполнять перемещение пропорционально dt.
 
-    b.x += bx;
-    b.y += by;
-    if (b.y > 580) {
-        b.y = 580;
-        by = -by;
-    } else if (b.y < 0) {
-        b.y = 0;
-        by = -by;
+    gs->b.x += gs->bx;
+    gs->b.y += gs->by;
+    if (gs->b.y > 580) {
+        gs->b.y = 580;
+        gs->by = -gs->by;
+    } else if (gs->b.y < 0) {
+        gs->b.y = 0;
+        gs->by = -gs->by;
     }
-    if (b.x <= 20 && b.y > p1.y && b.y < p1.y + p1.h) {
-        if (l) {
-            l = false;
-            bx = -bx;
+    if (gs->b.x <= 20 && gs->b.y > gs->p1.y && gs->b.y < gs->p1.y + gs->p1.h) {
+        if (gs->l) {
+            gs->l = false;
+            gs->bx = -gs->bx;
         }
-    } else if (b.x >= 960 && b.y > p2.y && b.y < p2.y + p2.h) {
-        if (l) {
-            l = false;
-            bx = -bx;
+    } else if (gs->b.x >= 960 && gs->b.y > gs->p2.y && gs->b.y < gs->p2.y + gs->p2.h) {
+        if (gs->l) {
+            gs->l = false;
+            gs->bx = -gs->bx;
         }
     } else {
-        l = true;
-        if (b.x < -20) {
-            b.x = 490;
-            b.y = 290;
-            bx = -bx;
-            c1++;
-        } else if (b.x > WINDOW_WIDTH) {
-            b.x = 490;
-            b.y = 290;
-            bx = -bx;
-            c2++;
+        gs->l = true;
+        if (gs->b.x < -20) {
+            gs->b.x = 490;
+            gs->b.y = 290;
+            gs->bx = -gs->bx;
+            gs->c1++;
+        } else if (gs->b.x > WINDOW_WIDTH) {
+            gs->b.x = 490;
+            gs->b.y = 290;
+            gs->bx = -gs->bx;
+            gs->c2++;
         }
     }
 }
@@ -204,30 +273,30 @@ static void moveBall(Uint32 dt) {
  *
  * @param dt время с момента последнего обновления (ms).
  */
-static void movePlayers(Uint32 dt) {
+static void movePlayers(const Uint32 dt, GameState *const gs) {
     const Uint8 *const keysDown = SDL_GetKeyboardState(NULL);
 
     if (keysDown[SDL_SCANCODE_W]) {
-        p1.y -= dt * 2 / 7;
-        if (p1.y < 0) {
-            p1.y = 0;
+        gs->p1.y -= dt * 2 / 7;
+        if (gs->p1.y < 0) {
+            gs->p1.y = 0;
         }
     } else if (keysDown[SDL_SCANCODE_S]) {
-        p1.y += dt * 2 / 7;
-        if (p1.y + p1.h > WINDOW_HEIGHT) {
-            p1.y = WINDOW_HEIGHT - p1.h;
+        gs->p1.y += dt * 2 / 7;
+        if (gs->p1.y + gs->p1.h > WINDOW_HEIGHT) {
+            gs->p1.y = WINDOW_HEIGHT - gs->p1.h;
         }
     }
 
     if (keysDown[SDL_SCANCODE_UP]) {
-        p2.y -= dt * 2 / 7;
-        if (p2.y < 0) {
-            p2.y = 0;
+        gs->p2.y -= dt * 2 / 7;
+        if (gs->p2.y < 0) {
+            gs->p2.y = 0;
         }
     } else if (keysDown[SDL_SCANCODE_DOWN]) {
-        p2.y += dt * 2 / 7;
-        if (p2.y + p2.h > WINDOW_HEIGHT) {
-            p2.y = WINDOW_HEIGHT - p2.h;
+        gs->p2.y += dt * 2 / 7;
+        if (gs->p2.y + gs->p2.h > WINDOW_HEIGHT) {
+            gs->p2.y = WINDOW_HEIGHT - gs->p2.h;
         }
     }
 }
@@ -239,16 +308,16 @@ static void movePlayers(Uint32 dt) {
  *
  * @param dt время с момента последнего обновления (ms).
  */
-static void update(Uint32 dt) {
-    movePlayers(dt);
-    moveBall(dt);
+static void updateGameState(const Uint32 dt, GameState *const gs) {
+    movePlayers(dt, gs);
+    moveBall(dt, gs);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
 // TODO: Документация к методу.
 
-static void gameLoop() {
+static void gameLoop(const RenderObjects *const ro, GameState *const gs) {
 
     Uint32 time = SDL_GetTicks();
     Uint32 dt = 1;
@@ -257,12 +326,10 @@ static void gameLoop() {
         bool shouldExit;
         pollSdlEvents(&shouldExit);
         if (shouldExit) {
-            SDL_DestroyWindow(window);
-            // TODO: освобождать прочие ресурсы перед завершением.
             return;
         }
-        redraw();
-        update(dt);
+        redraw(ro, gs);
+        updateGameState(dt, gs);
 
         Uint32 nextTime = SDL_GetTicks();
         dt = nextTime - time;
@@ -280,12 +347,19 @@ static void gameLoop() {
  *
  * @return код завершения процесса.
  */
-int main(int argc, char *argv[]) {
-    if (initGraphic()) {
+int main(const int argc, char **const args) {
+    RenderObjects ro;
+    if (initRenderObjects(&ro)) {
+        freeRenderObjects(&ro);
         return 1;
     }
-    initGame();
 
-    gameLoop();
+    GameState gs;
+    initGameState(&gs);
+
+    gameLoop(&ro, &gs);
+
+    freeRenderObjects(&ro);
+
     return 0;
 }
